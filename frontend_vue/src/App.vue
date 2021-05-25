4664af7b8ba31df57212c34fb57c89e7ba723a7e
Elena Bychkova <lena.bychkova.200012@gmail.com>
2021-05-25 15:17:14 +0300

    DEV:template for main page is ready

--- frontend_vue/src/App.vue
+++ frontend_vue/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="wrapper">
-    <nav class="navbar is-dark">
+    <nav class="navbar p-4 is-info">
       <div class="navbar-brand">
-        <router-link to="/" class="navbar-item"><strong>DoveCuments</strong></router-link>
+        <router-link to="/" class=" hov navbar-item is-size-4"><strong>DoveCuments</strong></router-link>
         <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu=!showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -11,25 +11,39 @@
         </div>
 
         <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
-        <div class="navbar-end">
-          <router-link to="/" class="navbar-item">DoveCuments</router-link>
-          <router-link to="/" class="navbar-item">DoveCuments</router-link>
-
-          <div class="navbar-item">
-            <div class="buttons">
-              <router-link to="/log-in" class="button is-light">Войти</router-link>
-            </div>
+        <div class="navbar-start">
+          <router-link to="/" class="navbar-item">Главная</router-link>
+          <router-link to="/courier" class="navbar-item">Курьерам</router-link>
+          <router-link to="/tariffs" class="navbar-item">Тарифы</router-link>
+          <router-link to="/contacts" class="navbar-item">Контакты</router-link>
           </div>
+          <div class="navbar-item navbar-end">
+            <div class="buttons">
+              <router-link to="/delivery-form" class="button is-rounded is-warning is-outlined">Заказать доставку</router-link>
+              <router-link to="/log-in" class="button is-rounded is-warning">Вход/Регистрация</router-link>
+            </div>
           </div>
           </div>
       </nav>
 
-  <section class="section">
   <router-view/>
-  </section>
 
   <footer class="footer">
-    <p>2021</p>
+  <div class="is-flex is-align-items-center is-justify-content-space-evenly">
+  <div class="content">
+    <p><strong>Телефон</strong></p>
+    <p>Почта</p>
+    <p>Адрес</p>
+  </div>
+  <div class="menu">
+  <ul class="menu-list">
+    <li><router-link to="/delivery-form" class="navbar-item">Заказать доставку</router-link></li>
+    <router-link to="/courier" class="navbar-item">Курьерам</router-link>
+    <router-link to="/tariffs" class="navbar-item">Тарифы</router-link>
+    <router-link to="/contacts" class="navbar-item">Контакты</router-link>
+  </ul>
+  </div>
+  </div>
   </footer>
   </div>
 </template>
@@ -46,4 +60,5 @@ export default {
 
 <style lang="scss">
 @import '../node_modules/bulma';
+
 </style>
--- frontend_vue/src/views/Home.vue
+++ frontend_vue/src/views/Home.vue
@@ -1,7 +1,64 @@
 <template>
-  <div class="home">
-  Home
+  <section class="section">
+  <div class="container is-fluid">
+  <div>
+  <h1 class="title has-text-info">Сервис курьерской доставки документов</h1>
+  <h2 class="title has-text-info is-2"><strong>Телефон</strong></h2>
+  <figure class="image">
+  <img src="../assets/dove.png" alt="голубь">
+  </figure>
   </div>
+
+
+
+  <div class="mt-6">
+  <h1 class="title has-text-info has-text-centered">Виды доставок</h1>
+  <div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      Card header
+    </p>
+      </header>
+    <div class="card-content">
+    <div class="content">
+      Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+    </div>
+  </div>
+</div>
+  <router-link to="/delivery-form" class="button is-rounded is-warning is-outlined">Заказать доставку</router-link>
+  </div>
+    </div>
+  </section>
+
+    <div class="mt-6 p-6 has-background-light">
+    <h1 class="title has-text-info has-text-centered">Необходимо оформлять заявки регулярно? <br> Зарегистрируйтесь!
+Или заполните короткую форму для разовой доставки на сайте.</h1>
+<div class="field is-grouped mt-6 is-flex is-justify-content-center">
+    <button class="button is-rounded is-warning mr-3">
+      Зарегистрироваться
+    </button>
+    <button class="button ml-6 is-rounded is-warning is-outlined">
+      Заказать доставку
+    </button>
+</div>
+    </div>
+    
+
+
+      <section class="section">
+  <div class="container is-fluid">
+  <div class="mt-6">
+  <h1 class="title has-text-info has-text-centered">Способы оплаты</h1>
+  <div class="card">
+  <header class="card-header">
+    <p class="card-header-title">
+      Card header
+    </p>
+      </header>
+</div>
+  </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -11,3 +68,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.image{
+  width: 20%;
+  margin-left:20%;
+}
+</style>
